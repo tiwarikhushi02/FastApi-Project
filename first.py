753183f11cd74e45f37fceca40f3420e445918f1
tiwarikhushi02 <khushitiwari0206@gmail.com>
2026-02-26 22:41:02 +0530

    update

--- first.py
+++ first.py
@@ -1,15 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
+import json
 
 app = FastAPI()
+def load_data():
+    with open('users.json','r') as f:
+        data = json.load(f)
+    return data
 
 @app.get("/")
 def hello():
-    return {"message": "hello world"}
+    return {"message": "User Management System"}
 
 @app.get("/about")
 def about():
-    return {"message": "this api project"}
+    return {"message": "Fully Functional API to manage user management System"}
 
 @app.get("/test")
 def test():
     return {"msg": "test working"}
+
+@app.get('/view')
+def view():
+    data = load_data()
+    return data['customers']
+
+@app.get('/view/{user_id}')
+def view_user(user_id: int = Path(..., description="The ID of the user to retrieve", example=1)):
+    data = load_data()
+    customers = data["customers"]   # access customers list
+
+    for user in customers:
+        if user["customer_id"] == user_id:
+            return user
+
+    return {"message": "User not found"}
